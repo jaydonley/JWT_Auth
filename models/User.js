ac6893c35cccf04cf55305c16e55d649eff87db5
Jason Donley <jay.donley@gmail.com>
2022-11-24 19:23:48 -0500

    day3Homework

--- models/User.js
+++ models/User.js
@@ -1,18 +1,33 @@
 const mongoose = require('mongoose');
-const mongooose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail,  'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
+});
+
+
+userSchema.post('save', function (doc, next) {
+
+    console.log('new user was created and saved', doc);
+    next();
+});
+
+userSchema.pre('save', async function (next) {
+    const salt  = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
